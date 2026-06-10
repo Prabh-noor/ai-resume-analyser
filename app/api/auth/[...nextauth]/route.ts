11d6c1e294a8a696cf406fcff5d0ae3c7a4e449d
Prabhnoor <prabhnoorkaur016@gmail.com>
2026-06-10 23:32:51 +0530

    Insert user entry to supabase

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,42 +1,5 @@
-// import NextAuth from "next-auth"
-// // import GithubProvider from "next-auth/providers/github"
-// import GoogleProvider from "next-auth/providers/google";
-
-// export const authOptions = {
-//   // Configure one or more authentication providers
-//   providers: [
-//     // GithubProvider({
-//     //   clientId: process.env.GITHUB_ID,
-//     //   clientSecret: process.env.GITHUB_SECRET,
-//     // }),
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_SECRET,
-//       authorization: {
-//         params: {
-//           prompt: "consent",
-//           access_type: "offline",
-//           response_type: "code"
-//         }
-//       }
-//     })
-//     // ...add more providers here
-//   ],
-//   callbacks: {
-//     async signIn({ account, profile }) {
-//       console.log("Next auth callback -->>>", account, profile);
-//       if (account.provider === "google") {
-//         // return profile.email_verified && profile.email.endsWith("@example.com")
-//       }
-//       return true // Do different verification for other providers that don't have `email_verified`
-//     },
-//   }
-// }
-
-
-
-// export default NextAuth(authOptions)
-
+// import { supabase } from "@/app/lib/supabase";
+import { supabaseAdmin } from "@/app/lib/supabase-admin";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -50,6 +13,20 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      console.log("User Signed In", user);
+      // Fires every time user logs in
+      const response = await supabaseAdmin.from("users").upsert({
+        email: user.email,
+        fullname: user.name,
+        image: user.image,
+        auth_type: 'google'
+      });
+      console.log("Supabase response", response);
+      return true; // returning true allows the sign in to proceed otherwise access denied page gets shown
+    },
+  }
 });
 
 export { handler as GET, handler as POST };
